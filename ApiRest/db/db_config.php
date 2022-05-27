fa6d5d28a1580e3beec1bf1a0e45b1a2c2e3e892
Juan Zabala <juanzabaladev@gmail.com>
2022-05-26 19:32:26 -0500

    added index.php

--- ApiRest/db/db_config.php
+++ ApiRest/db/db_config.php
@@ -1,18 +1,19 @@
 <?php
 class DBConfig{
-
-    private $user = "root";
-    private $password = "";
+    private $user = "admin";
+    private $password = "cesdemoviles2jueves";
     private $dbName = "delivery_app";
-    private $host = "localhost";
+    private $host = "cesde-moviles2-jueves.cilulcrjbtir.us-east-1.rds.amazonaws.com";
+    private $port = "3306";
 
     public function connect(){
         try{
-            $dsn = "mysql:host=$this->host;dbname=$this->dbName";
+            $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->dbName";
             $connection = new PDO($dsn,$this->user, $this->password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $connection;
             echo "Conexión exitosa";
+            return $connection;
+
         }catch(PDOException $exception){
             echo "Error en la db". $exception->getMessage();
         }
